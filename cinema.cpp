8d1cc9c8bbe652dd1b0c14e487959867325d3aa6
gophy5128 <gophy5128@cgmh.org.tw>
2021-12-05 20:43:36 +0800

    foodstand

--- cinema.cpp
+++ cinema.cpp
@@ -1,1 +1,44 @@
+#include<cstdio>
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+
+class cinema
+{
+public:
+    virtual void () = 0;
+};
+
+class Coke: public food
+{
+public:
+    int price() {
+        return 25;
+    }
+};
+
+class Blacktea: public food
+{
+public:
+    int price() {
+        return 30;
+    }
+};
+
+class Popcorn: public food
+{
+public:
+    int price() {
+        return 50;
+    }
+};
+class Churros: public food
+{
+public:
+    int price() {
+        return 50;
+    }
+};
 
--- foodstand.cpp
+++ foodstand.cpp
@@ -1,1 +1,48 @@
+#include<cstdio>
+#include<iostream>
+#include<vector>
+#include<string>
+#include<food.h>
+using namespace std;
 
+class foodstand
+{
+public:
+    static food* create(int p){
+        if (p == 1) {
+            food* f = new Coke;
+            return f;
+        }
+        else if (p == 2) {
+            food* f = new Blacktea;
+            return f;
+        }
+        else if (p == 3) {
+            food* f = new Popcorn;
+            return f;
+        }
+        else if (p == 3) {
+             food* f = new Churros;
+            return f;
+        }
+    }
+    int Buy(string f){
+        int p;
+        if (f == "Coke"){
+            food* order = foodstand::create(1);
+            return order->price;
+        }
+        else if (f == "Black tea"){
+            food* order = foodstand::create(2);
+            return order->price;
+        }
+        else if (f == "Popcorn"){
+            food* order = foodstand::create(3);
+            return order->price;
+        }
+        else if (f == "Churros"){
+            food* order = foodstand::create(4);
+            return order->price;
+        }
+    }
+}
