82204bdb1a1dad7170999616e3d70e8889a70036
pingchungchang <83915045+pingchungchang@users.noreply.github.com>
2021-12-12 15:38:36 +0800

    finish customer.cpp

--- customer.cpp
+++ customer.cpp
@@ -1,1 +1,36 @@
+#include "others.h"
+#include "dispatcher.h"
+#include "saleman.h"
+#include <movie.h>
+using namespace std;
+customer::customer(string c_name,string c_id,int c_age){
+    name = c_name;
+    id = c_id;
+    age = c_age;
+}
 
+customer::vector<package*> Search_By_Movie(string movie_name){
+	dispatcher* d = dispatcher::Get_Instance();
+	salesman* seller = dispatcher->Match(this);
+	return salesman->Search_By_Movie(movie_name);
+}
+customer::vector<package*> Search_By_Time(time){
+	dispatcher* d = dispatcher::Get_Instance();
+	salesman* seller = dispatcher->Match(this);
+	return salesman->Search_By_Time(t);	
+}
+customer::vector<package*> Search_By_Type(int cinema){
+	dispatcher* d = dispatcher::Get_Instance();
+	salesman* seller = dispatcher->Match(this);
+	return salesman->Search_By_Time(cinema);	
+}
+customer::int Buy_Food(string food_name){
+	dispatcher* d = dispatcher::Get_Instance();
+	salesman* seller = dispatcher->Match(self);
+	return salesman->Buy_food(this,food_name);	
+}
+customer::ticketinfo Buy_Ticket(string movie_name,int cinema_id,time t){
+	dispatcher* d = dispatcher::Get_Instance();
+	salesman* seller = dispatcher->Match(this);
+	return salesman->Buy_Ticket(this,movie_name,cinema_id,t);	
+}
--- customer.h
+++ customer.h
@@ -5,12 +5,13 @@ using namespace std;
 
 class customer{
 public:
+    customer(string c_name,string c_id,int c_age);
     string name;
     string id;
     int age;
     vector<package*> Search_By_Movie(string movie_name);
-    vector<package*> Search_By_Time(time);
+    vector<package*> Search_By_Time(time t);
     vector<package*> Search_By_Type(int cinema);
-    int Buy_Food(customer* customer_pointer,string food_name);
-    ticketinfo Buy_Ticket(customer* customer_pointer,string movie_name,int cinema_id,time t);
+    int Buy_Food(string food_name);
+    ticketinfo Buy_Ticket(string movie_name,int cinema_id,time t);
 };
